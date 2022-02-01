a1653dced9ec3d7bb2469afa1c780ee10fddb726
Yoan Thirion <yoan.thirion@gmail.com>
2022-02-01 19:41:11 +0100

    Revert "add either"
    This reverts commit 3c3e6a0556f4b324931c740e8b48795023e17ae9.

--- language-ext-kata-tests/EitherExercises.cs
+++ language-ext-kata-tests/EitherExercises.cs
@@ -14,11 +14,12 @@ public class EitherExercises
     {
         // Divide x = 9 by y = 2
         var eitherResult = Divide(9, 2);
-        var result = eitherResult.IfLeft(0);
+        int result = 0;
 
-        eitherResult.IsRight.Should().BeTrue();
-        eitherResult.IsLeft.Should().BeFalse();
         result.Should().Be(4);
+        eitherResult.IsRight.Should().BeTrue();
+        eitherResult.IsDefault().Should().BeFalse();
+        eitherResult.IsLeft.Should().BeFalse();
     }
 
     [Fact]
@@ -26,9 +27,7 @@ public class EitherExercises
     {
         // Divide x = 9 by y = 2 and add z to the result
         int z = 3;
-        int result = Divide(9, 2)
-            .Map(a => a + z)
-            .IfLeft(0);
+        int result = 0;
 
         result.Should().Be(7);
     }
@@ -37,7 +36,7 @@ public class EitherExercises
     public void DivideByZeroIsAlwaysAGoodIdea()
     {
         // Divide x by 0 and get the result
-        Func<Either<Error, int>> call = () => Divide(1, 0);
+        Func<Either<Error, int>> call = null;
         var result = call.Invoke();
 
         result.IsLeft.Should().BeTrue();
@@ -49,7 +48,7 @@ public class EitherExercises
     {
         // Divide x by 0, on exception returns 0
         int x = 1;
-        int result = Divide(x, 0).IfLeft(0);
+        int result = -1;
 
         result.Should().Be(0);
     }
@@ -60,14 +59,24 @@ public class EitherExercises
         // Divide x by 0, log the failure message to the console and get 0
         int x = 1;
 
-        int result = Divide(x, 0)
-            .IfLeft(left =>
-            {
-                Console.WriteLine(left.Message);
-                return 0;
-            });
+        int result = -1;
 
         result.Should().Be(0);
+    }
+
+    [Fact]
+    public void MapTheSuccess()
+    {
+        // Divide x by y
+        // log the failure message to the console
+        // Log your success to the console
+        // Get the result or 0 if exception
+        int x = 8;
+        int y = 4;
+
+        var result = -1;
+
+        result.Should().Be(2);
     }
 
     [Fact]
@@ -81,15 +90,7 @@ public class EitherExercises
         int x = 27;
         int y = 3;
 
-        int result = Divide(x, y)
-            .Bind(previous => Divide(previous, y))
-            .Bind(previous => Divide(previous, y))
-            .Do(success => Console.WriteLine(success))
-            .IfLeft(left =>
-            {
-                Console.WriteLine(left.Message);
-                return 0;
-            });
+        int result = -1;
 
         result.Should().Be(1);
     }
