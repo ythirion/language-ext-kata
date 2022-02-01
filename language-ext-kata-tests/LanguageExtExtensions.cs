255795faacb8baeb20c1aadcfa0dcf5b6e098de3
Yoan Thirion <yoan.thirion@gmail.com>
2022-02-01 18:44:40 +0100

    add either exercise

--- language-ext-kata-tests/LanguageExtExtensions.cs
+++ language-ext-kata-tests/LanguageExtExtensions.cs
@@ -7,4 +7,7 @@ public static class LanguageExtExtensions
 {
     public static T GetUnsafe<T>(this Option<T> option)
         => option.IfNone(() => throw new ArgumentException("Can not Get from None"));
+
+    public static TLeft LeftUnsafe<TLeft, TRight>(this Either<TLeft, TRight> either)
+        => either.LeftToSeq().SingleOrDefault();
 }
